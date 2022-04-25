8050f76e46b55c9a9b1453a4d92c61b8dec57ad5
CristianGM <cristiangm@gmail.com>
2022-04-25 16:28:25 +0200

    Simplify APK setup

--- src/main/kotlin/com/github/flank/gradle/tasks/FlankDoctorTask.kt
+++ src/main/kotlin/com/github/flank/gradle/tasks/FlankDoctorTask.kt
@@ -1,24 +1,18 @@
 package com.github.flank.gradle.tasks
 
 import javax.inject.Inject
-import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.ProjectLayout
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.*
 import org.gradle.process.ExecOperations
 
-abstract class FlankDoctorTask : DefaultTask() {
+abstract class FlankDoctorTask : BaseFlankApkTask() {
   @get:Inject protected abstract val projectLayout: ProjectLayout
   @get:Inject protected abstract val execOperations: ExecOperations
 
   @get:Input abstract val variant: Property<String>
-
-  @get:InputFile @get:Classpath @get:Optional abstract val appApk: RegularFileProperty
-  @get:InputDirectory @get:Classpath @get:Optional abstract val appApkDir: DirectoryProperty
-  @get:InputDirectory @get:Classpath abstract val testApkDir: DirectoryProperty
 
   @get:InputFiles @get:Classpath abstract val flankJarClasspath: ConfigurableFileCollection
 
@@ -33,9 +27,6 @@ abstract class FlankDoctorTask : DefaultTask() {
 
   @TaskAction
   fun run() {
-    check(appApk.isPresent xor appApkDir.isPresent) {
-      "One, and only one, of appApk or appApkDir must be set"
-    }
     execOperations
         .javaexec {
           classpath = flankJarClasspath
